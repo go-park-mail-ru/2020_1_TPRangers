3ef2990366945916970f26184f2f0555d5717df7
znjuko <znajderko@yandex.ru>
2020-04-14 13:41:42 +0300

    more tests on friend repos

--- internal/friends/repository/friends_repository.go
+++ internal/friends/repository/friends_repository.go
@@ -24,27 +24,28 @@ func (Data FriendRepositoryRealisation) GetUserLoginById(userId int) (string, er
 	err := row.Scan(&login)
 
 	return login, err
-}
+} //
 
 func (Data FriendRepositoryRealisation) GetUserFriendsById(id, friendsCount int) ([]models.FriendLandingInfo, error) {
 	userFriends := make([]models.FriendLandingInfo, 0, 6)
 
 	row, err := Data.friendDB.Query("select name, url , login from friends F inner join users U on F.f_id=U.u_id INNER JOIN photos P ON U.photo_id=P.photo_id "+
 		"WHERE F.u_id=$1 GROUP BY F.u_id,F.f_id,U.u_id,P.photo_id LIMIT $2", id, friendsCount)
-	defer row.Close()
+	defer func () {
+		if row != nil {
+			row.Close()
+		}
+	}()
 
 	if err != nil {
 		return nil, errors.FailReadFromDB
 	}
-
 	for row.Next() {
 
 		var friendInfo models.FriendLandingInfo
 
 		err = row.Scan(&friendInfo.Name, &friendInfo.Photo, &friendInfo.Login)
-
 		if err != nil {
-			fmt.Println(err)
 			return nil, errors.FailReadToVar
 		}
 
@@ -53,7 +54,7 @@ func (Data FriendRepositoryRealisation) GetUserFriendsById(id, friendsCount int)
 	}
 
 	return userFriends, nil
-}
+} //
 
 func (Data FriendRepositoryRealisation) GetAllFriendsByLogin(login string) ([]models.FriendLandingInfo, error) {
 	userFriends := make([]models.FriendLandingInfo, 0, 20)
@@ -66,7 +67,11 @@ func (Data FriendRepositoryRealisation) GetAllFriendsByLogin(login string) ([]mo
 
 	row, err := Data.friendDB.Query("select name, url , login , surname from friends F inner join users U on F.f_id=U.u_id INNER JOIN photos P ON U.photo_id=P.photo_id "+
 		"WHERE F.u_id=$1 GROUP BY F.u_id,F.f_id,U.u_id,P.photo_id", id)
-	defer row.Close()
+	defer func () {
+		if row != nil {
+			row.Close()
+		}
+	}()
 
 	if err != nil {
 		return nil, errors.FailReadFromDB
@@ -101,7 +106,7 @@ func (Data FriendRepositoryRealisation) GetUserFriendsByLogin(login string, frie
 	}
 
 	return Data.GetUserFriendsById(id, friendsCount)
-}
+} //
 
 func (Data FriendRepositoryRealisation) AddFriend(firstFriend, secondFriend int) error {
 	var err error
@@ -112,12 +117,12 @@ func (Data FriendRepositoryRealisation) AddFriend(firstFriend, secondFriend int)
 		err = errors.FailAddFriend
 	}
 	return err
-}
+} //
 
 func (Data FriendRepositoryRealisation) DeleteFriend(firstFriend, secondFriend int) error {
 	_, err := Data.friendDB.Exec("DELETE FROM Friends WHERE ((u_id = $1 AND f_id = $2) OR (u_id = $2 AND f_id = $1))", firstFriend, secondFriend)
 	return err
-}
+} //
 
 func (Data FriendRepositoryRealisation) GetFriendIdByLogin(login string) (int, error) {
 	var friend_id int
@@ -127,23 +132,21 @@ func (Data FriendRepositoryRealisation) GetFriendIdByLogin(login string) (int, e
 	scanErr := row.Scan(&friend_id)
 
 	return friend_id, scanErr
-}
+} //
 
 func (Data FriendRepositoryRealisation) GetIdByLogin(login string) (int, error) {
 
 	var i *int
 
-	row := Data.friendDB.QueryRow("select users.u_id from users where users.login = $1", login)
+	row := Data.friendDB.QueryRow("SELECT users.u_id FROM users WHERE users.login = $1", login)
 
 	err := row.Scan(&i)
 	if err != nil {
-		fmt.Println(err.Error())
-
 		return 0, err
 	}
 
 	return *i, err
-}
+} //
 
 func (Data FriendRepositoryRealisation) CheckFriendship(id1, id2 int) (bool, error) {
 	row := Data.friendDB.QueryRow("SELECT f_id FROM friends WHERE u_id=$1 AND f_id=$2", id1, id2)
@@ -162,4 +165,4 @@ func (Data FriendRepositoryRealisation) CheckFriendship(id1, id2 int) (bool, err
 
 	return true, nil
 
-}
+} //
