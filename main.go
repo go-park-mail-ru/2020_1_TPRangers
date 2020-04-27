b0be6123ffd3688d43a6a8177062932dc6aca6b0
znjuko <znajderko@yandex.ru>
2020-04-27 14:44:32 +0300

    chat + micro

--- main.go
+++ main.go
@@ -8,21 +8,10 @@ import (
 	"go.uber.org/zap/zapcore"
 	"google.golang.org/grpc"
 
-	repositoryMessage "main/internal/message/repository"
-	deliveryMessage "main/internal/socket/delivery"
-	usecaseMessage "main/internal/socket/usecase"
-
-	deliveryToken "main/internal/socket_token/delivery"
-	repositoryToken "main/internal/socket_token/repository"
-	usecaseToken "main/internal/socket_token/usecase"
-
-	deliveryChat "main/internal/chats/delivery"
-	repositoryChat "main/internal/chats/repository"
-	usecaseChat "main/internal/chats/usecase"
-
 	deliveryAlbum "main/internal/albums/delivery"
 	repositoryAlbum "main/internal/albums/repository"
-	repositoryCookie "main/internal/cookies/repository"
+	repositoryChat "main/internal/chats/repository"
+	usecaseChat "main/internal/chats/usecase"
 	deliveryFeed "main/internal/feeds/delivery"
 	repositoryFeed "main/internal/feeds/repository"
 	usecaseFeed "main/internal/feeds/usecase"
@@ -48,19 +37,15 @@ import (
 )
 
 type RequestHandlers struct {
-	userHandler        deliveryUser.UserDeliveryRealisation
-	feedHandler        deliveryFeed.FeedDeliveryRealisation
-	likeHandler        deliveryLikes.LikeDelivery
-	photoHandler       deliveryPhoto.PhotoDeliveryRealisation
-	albumHandler       deliveryAlbum.AlbumDeliveryRealisation
-	friendHandler      deliveryFriends.FriendDeliveryRealisation
-	messageHandler     deliveryMessage.SocketDelivery
-	chatHandler        deliveryChat.ChatsDelivery
-	socketTokenHandler deliveryToken.TokenDelivery
+	userHandler   deliveryUser.UserDeliveryRealisation
+	feedHandler   deliveryFeed.FeedDeliveryRealisation
+	likeHandler   deliveryLikes.LikeDelivery
+	photoHandler  deliveryPhoto.PhotoDeliveryRealisation
+	albumHandler  deliveryAlbum.AlbumDeliveryRealisation
+	friendHandler deliveryFriends.FriendDeliveryRealisation
 }
 
-func InitializeDataBases(server *echo.Echo) (*sql.DB, repositoryCookie.CookieRepositoryRealisation, repositoryMessage.MessageRepositoryRealisation,
-	repositoryToken.TokenRepositoryRealisation) {
+func InitializeDataBases(server *echo.Echo) *sql.DB {
 	err := godotenv.Load("project.env")
 	if err != nil {
 		server.Logger.Fatal("can't load .env file :", err.Error())
@@ -76,20 +61,10 @@ func InitializeDataBases(server *echo.Echo) (*sql.DB, repositoryCookie.CookieRep
 		server.Logger.Fatal("NO CONNECTION TO BD", err.Error())
 	}
 
-	redisPas := os.Getenv("REDIS_PASSWORD")
-	redisPort := os.Getenv("REDIS_PORT")
-
-	redisChatPort := os.Getenv("REDIS_CHAT_PORT")
-
-	sessionDB := repositoryCookie.NewCookieRepositoryRealisation(redisPort, redisPas)
-	tokenDB := repositoryToken.NewTokenRepositoryRealisation(redisPort, redisPas)
-	chatDB := repositoryMessage.NewMessageRepositoryRealisation(redisChatPort, "", db)
-
-	return db, sessionDB, chatDB, tokenDB
+	return db
 }
 
-func NewRequestHandler(db *sql.DB, session authorMicro.SessionCheckerClient, messageDB repositoryMessage.MessageRepositoryRealisation,
-	tokenDB repositoryToken.TokenRepositoryRealisation, logger *zap.SugaredLogger) *RequestHandlers {
+func NewRequestHandler(db *sql.DB, session authorMicro.SessionCheckerClient, logger *zap.SugaredLogger) *RequestHandlers {
 
 	feedDB := repositoryFeed.NewFeedRepositoryRealisation(db)
 	userDB := repositoryUser.NewUserRepositoryRealisation(db)
@@ -97,48 +72,37 @@ func NewRequestHandler(db *sql.DB, session authorMicro.SessionCheckerClient, mes
 	photoDB := repositoryPhoto.NewPhotoRepositoryRealisation(db)
 	likesDB := repositoryLikes.NewLikeRepositoryRealisation(db)
 	albumDB := repositoryAlbum.NewAlbumRepositoryRealisation(db)
-
 	friendsDB := repositoryFriends.NewFriendRepositoryRealisation(db)
 
 	photoUseCase := usecasePhoto.NewPhotoUseCaseRealisation(photoDB)
 	albumUseCase := usecaseAlbum.NewAlbumUseCaseRealisation(albumDB)
-	messageUseCase := usecaseMessage.NewSocketUseCaseRealisation(messageDB, tokenDB)
 	feedUseCase := usecaseFeed.NewFeedUseCaseRealisation(feedDB)
 	userUseCase := usecaseUser.NewUserUseCaseRealisation(userDB, friendsDB, feedDB, session)
 	likesUse := usecaseLikes.NewLikeUseRealisation(likesDB)
 	friendsUse := usecaseFriends.NewFriendUseCaseRealisation(friendsDB)
 	chatUse := usecaseChat.NewChatUseCaseRealisation(chatDB, friendsDB)
-	socketTokenUse := usecaseToken.NewTokenUseCaseRealisation(tokenDB)
 
 	likeH := deliveryLikes.NewLikeDelivery(logger, likesUse)
-
 	userH := deliveryUser.NewUserDelivery(logger, userUseCase)
 	feedH := deliveryFeed.NewFeedDelivery(logger, feedUseCase)
-	messageH := deliveryMessage.NewSocketDelivery(logger, messageUseCase)
 	photoH := deliveryPhoto.NewPhotoDelivery(logger, photoUseCase)
 	albumH := deliveryAlbum.NewAlbumDelivery(logger, albumUseCase)
-	chatH := deliveryChat.NewChatsDelivery(logger, chatUse)
-	socketTokenH := deliveryToken.NewTokenDelivery(logger, socketTokenUse)
-
 	friendH := deliveryFriends.NewFriendDelivery(logger, friendsUse, chatUse)
 
 	api := &(RequestHandlers{
 
-		photoHandler:       photoH,
-		albumHandler:       albumH,
-		userHandler:        userH,
-		feedHandler:        feedH,
-		likeHandler:        likeH,
-		friendHandler:      friendH,
-		messageHandler:     messageH,
-		chatHandler:        chatH,
-		socketTokenHandler: socketTokenH,
+		photoHandler:  photoH,
+		albumHandler:  albumH,
+		userHandler:   userH,
+		feedHandler:   feedH,
+		likeHandler:   likeH,
+		friendHandler: friendH,
 	})
 
 	return api
 }
 
-func LoadMicroservices(server *echo.Echo) (authorMicro.SessionCheckerClient , *grpc.ClientConn) {
+func LoadMicroservices(server *echo.Echo) (authorMicro.SessionCheckerClient, *grpc.ClientConn) {
 
 	authPORT := os.Getenv("AUTHORIZ_PORT")
 
@@ -150,10 +114,9 @@ func LoadMicroservices(server *echo.Echo) (authorMicro.SessionCheckerClient , *g
 		server.Logger.Fatal("cant connect to grpc")
 	}
 
-
 	authManager := authorMicro.NewSessionCheckerClient(grpcConn)
 
-	return authManager , grpcConn
+	return authManager, grpcConn
 
 }
 
@@ -167,27 +130,27 @@ func main() {
 	logger := prLogger.Sugar()
 	defer prLogger.Sync()
 
-	db, _, messages, tokens := InitializeDataBases(server)
+	db := InitializeDataBases(server)
 	defer func() {
 		if db != nil {
 			db.Close()
 		}
-	} ()
+	}()
 
 	origin := os.Getenv("ORIGIN_POLICY")
 
-	auth , authConn := LoadMicroservices(server)
+	auth, authConn := LoadMicroservices(server)
 
-	defer func () {
+	defer func() {
 		if authConn != nil {
 			authConn.Close()
 		}
-	} ()
+	}()
 
 	midHandler := middleware.NewMiddlewareHandler(logger, auth, origin)
 	midHandler.SetMiddleware(server)
 
-	api := NewRequestHandler(db, auth, messages, tokens, logger)
+	api := NewRequestHandler(db, auth, logger)
 
 	api.userHandler.InitHandlers(server)
 	api.feedHandler.InitHandlers(server)
@@ -195,9 +158,6 @@ func main() {
 	api.photoHandler.InitHandlers(server)
 	api.albumHandler.InitHandlers(server)
 	api.friendHandler.InitHandlers(server)
-	api.messageHandler.InitHandlers(server)
-	api.chatHandler.InitHandlers(server)
-	api.socketTokenHandler.InitHandlers(server)
 
 	port := os.Getenv("PORT")
 
