e9a9dbd82ae1422dc7fde4744a3e0c2f7c4e7354
znjuko <znajderko@yandex.ru>
2020-04-29 01:45:10 +0300

    added grafana cfg

--- metrics/metrics.sh
+++ metrics/metrics.sh
@@ -17,4 +17,4 @@ sudo docker run -p 9090:9090 -d --name prometheus --net=host -v $PWD/prometheus:
 echo "running node exporter on port 9100"
 sudo docker run -p 9100:9100 -d --name node_exporter --net=host -v $PWD/node_exporter:/etc/config prom/node-exporter --path.rootfs=/etc/config
 echo "running grafana on port 3000"
-sudo docker run -d -p 3000:3000 --name grafana --net=host -v $PWD/grafana:/var/lib/grafana grafana/grafana
+sudo docker run -d -p 3000:3000 --name grafana --net=host -v $PWD/grafana/grafana.ini:/etc/grafana/grafana.ini grafana/grafana
