d038a0aedb8748695e0cf1abc3c1bf77ae7597fd
root <root@images.os.selectel.org>
2020-04-29 15:19:45 +0000

    prod ready

--- starter.sh
+++ starter.sh
@@ -1,20 +1,22 @@
-#!/bin/zsh
+#!/bin/bash
 
 runner_func() {
   echo "cd to $1"
   # shellcheck disable=SC2164
   cd $1
   go build main.go
-  nohup ./main >> server.log &
-  sleep 1
+  nohup ./main > server.log &
+  if [[ "$1" != "." ]]; then
+    cd ../../../
+  fi  
 }
 
 # shellcheck disable=SC2140
 # shellcheck disable=SC1083
 for services in "." "internal/cmd/authorization/" "internal/cmd/chats/" "internal/cmd/likes/" "internal/cmd/photos/"; do
-  runner_func "$services" &
+  runner_func "$services" 
 done
-wait
+
 
 
 echo "all services are running"
