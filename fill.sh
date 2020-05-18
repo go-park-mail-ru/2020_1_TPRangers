0b7d3e34e23938f6d2f9b8ef08298f29818ed4ba
alexey ershkov <ershkov.alex2000@gmail.com>
2020-05-18 17:51:14 +0300

    new stickers added

--- fill.sh
+++ fill.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-echo "INSERT INTO packs (pack_id, author) VALUES (1,1);"
+echo "INSERT INTO packs (pack_id, author,name) VALUES (1,1,'pack1');"
 for (( i=1; i<48; i++ )) 
 do
 echo "INSERT INTO stickers(stick_id, pack_id, sticker_link) VALUES ($i, 1, 'https://social-hub.ru/uploads/stickers/$i.png');"
 echo "INSERT INTO packstickers(stick_id, pack_id) VALUES ($i, 1);" 
+done
+echo "INSERT INTO packs (pack_id, author,name) VALUES (2,1,'pack2');"
+for (( i=48; i<62; i++ ))
+do
+echo "INSERT INTO stickers(stick_id, pack_id, sticker_link) VALUES ($i, 2, 'https://social-hub.ru/uploads/stickers/$i.png');"
+echo "INSERT INTO packstickers(stick_id, pack_id) VALUES ($i, 2);"
 done
